d327376892c6a4129e81ebbe6d8162342774ef40
Alain Ng <alain.ng@koolicar.com>
2017-01-06 16:32:12 -0500

    test6

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
 # write your code here
+	attr_reader :title
+
+	def title=(new_title)
+		escapes=%w(the and or in a an of)
+		words = new_title.split(" ")
+		results = [words[0].capitalize] + 
+			words[1..-1].map do |word|
+				if escapes.include?(word)
+					word
+				else
+					word.capitalize
+				end
+			end
+		@title = results.join(' ')
+	end
+
 end
