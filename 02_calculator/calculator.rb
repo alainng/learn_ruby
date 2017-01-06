0aead69eee817e13990108fc5335135c13eda232
Alain Ng <alain.ng@koolicar.com>
2017-01-06 14:23:44 -0500

    test

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add(first,second)#write your code here
+	return first+second
+end
+
+def subtract(first,second)
+	return first-second
+end
+
+def sum(array)
+	result = 0
+	array.each do |value|
+		result += value
+	end
+	return result
+end
+
+def multiply(arg1,arg2,*rest)
+	result = arg1*arg2
+	rest.each do |value|
+		result = result * value
+	end
+	return result
+end
+
+def power(arg1,arg2)
+	result = arg1**arg2
+	return result
+end
+
+def factorial(number)
+	if number == 0
+		return 1
+	else
+		return number*factorial(number-1)
+	end
+end
